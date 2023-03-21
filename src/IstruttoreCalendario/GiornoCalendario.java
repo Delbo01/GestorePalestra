21b2fd824917bba30f943ebb8372af7262088e18
cristianonarducci <cristiano.narducci@stud.unifi.it>
2023-03-21 11:15:35 +0100

    *cambiato tipo di lista

--- src/IstruttoreCalendario/GiornoCalendario.java
+++ src/IstruttoreCalendario/GiornoCalendario.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GiornoCalendario {
-    private List<Corso> corsi = new ArrayList<>();
+    private ArrayList<Corso> corsi = new ArrayList<>();
     private int giorno;
     public GiornoCalendario(int giorno) {
         this.giorno = giorno;
